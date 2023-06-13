2609490e325c58fe367f20cc7a604f8e42964d76
timur <timur@gmail.com>
2023-06-13 12:32:40 +1200

    Zadanie1 сохраняем

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число x и y");
+int x = int.Parse(Console.ReadLine());
+int y = int.Parse(Console.ReadLine());
+if (x > y)
+Console.WriteLine("Число х > y");
+else if (y > x) Console.WriteLine("Число y > x");
+else Console.WriteLine("Числа равны");
