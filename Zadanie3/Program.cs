1f7fb2cfc9b5363159b64145c30a86f95226598b
timur <timur@gmail.com>
2023-06-13 14:01:13 +1200

    Zadanie3

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число x");
+int x = int.Parse(Console.ReadLine());
+if (x % 2 == 0)
+Console.WriteLine("Число х число четное");
+else Console.WriteLine("Число нечетное");
