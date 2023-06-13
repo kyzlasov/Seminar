2119adc52b43e3d2d41c5e5e4a47305bb9fdbb3f
timur <timur@gmail.com>
2023-06-13 14:35:53 +1200

    Zadanie4

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Задайте положительное число");
+int num = int.Parse(Console.ReadLine());
+int count = 2;
+while (count < num)
+{
+    if (num % 2 == 0)
+    Console.WriteLine(count);
+    count = count + 2;
+}
