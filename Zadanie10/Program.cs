1f14cfe3402ce2913fbbc68769aec2e3126817ca
timur <timur@gmail.com>
2023-06-18 11:25:24 +1200

    Zadanie10

--- Zadanie10/Program.cs
+++ Zadanie10/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int num1 = num / 10;
+int num2 = num1 % 10;
+Console.WriteLine($"Второе число = {num2}");
