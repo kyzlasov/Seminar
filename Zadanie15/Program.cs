a8c0b54f58872d5f943c1d54ff6b2f2ce3339a85
timur <timur@gmail.com>
2023-06-18 13:18:21 +1200

    Zadanie15

---
+++ Zadanie15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число недели");
+int chislo = int.Parse(Console.ReadLine());
+if (chislo > 0 && chislo < 6){
+    Console.WriteLine("Рабочий день");
+}
+else if (chislo == 6 || chislo == 7){
+    Console.WriteLine("Выходной");
+}
+else Console.WriteLine("Введено неверное число");
