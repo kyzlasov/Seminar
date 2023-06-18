1a4434cf7d607919c5a5089f3b5b5d6b71389a9d
timur <timur@gmail.com>
2023-06-18 13:27:36 +1200

    Zadanie21

---
+++ Zadanie21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значение X1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение X2");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение y2");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение z1");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение z2");
+int z2 = int.Parse(Console.ReadLine());
+double n = Math.Sqrt(Math.Pow(x2-x1,2)) + Math.Pow(y2-y1,2) + Math.Pow(z2-z1,2);
+Console.WriteLine($"Расстояние в 3D пространстве n = {n:f2}");
