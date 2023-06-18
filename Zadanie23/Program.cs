f14cc76128ff68fab533b6867753d7aba529652e
timur <timur@gmail.com>
2023-06-18 13:39:17 +1200

    Zadanie23

---
+++ Zadanie23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число сколько раз возвести в куб");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+if (N < 0){
+    Console.WriteLine("Введите положительное число");
+}
+while (i <= N)
+{
+    Console.WriteLine(Math.Pow(i,3));
+    i++;
+}
+
