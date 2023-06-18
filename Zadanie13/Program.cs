1416165d6191ec164323687b99fd92dc049de3e6
timur <timur@gmail.com>
2023-06-18 13:10:07 +1200

    Zadanie13

---
+++ Zadanie13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num <= 99){
+    Console.WriteLine("Число меньше ста");
+    return;
+} 
+while (num > 1000)
+    num = num/10;
+Console.WriteLine($"Третье число {num%10}");
