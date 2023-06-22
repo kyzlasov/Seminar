e45020d98bf9fd2a1d7c2896b3093282136511ac
timur <timur@gmail.com>
2023-06-23 10:58:45 +1200

    Zadaie19

---
+++ Zadanie19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int i = int.Parse(Console.ReadLine());
+if (i < 10000 || i > 99999) 
+{
+    Console.WriteLine("Ввели не пятизначное число");
+
+}
+if (i / 10000 == i % 10)
+{
+    if (i/1000%10 == i % 100/10)
+    {
+        Console.WriteLine("Число палнидром");
+    }
+    else Console.WriteLine("Число не палиндром");
+}
+else Console.WriteLine("Число не палиндром");
+
+
