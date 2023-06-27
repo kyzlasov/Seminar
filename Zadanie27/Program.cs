f4ee868f54586cc211178a8a25f16a19dc7c69c7
timur <timur@gmail.com>
2023-06-27 15:01:50 +1200

    Zadanie27

---
+++ Zadanie27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int result = Suma(i);
+Console.WriteLine(result);
+
+
+int Suma(int i)
+{
+    int sum = 0;
+    while( i >  0)
+    {
+        int num = i % 10;
+        i = i / 10;
+        sum = sum + num;
+    }
+    return sum;
+}
+
+
+
+
+
