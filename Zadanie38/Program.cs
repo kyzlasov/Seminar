ce357fea352b1a4de4e0ee99b331e0652ab06e11
timur <timur@gmail.com>
2023-07-06 13:48:19 +1200

    Zadanie38

---
+++ Zadanie38/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76  */
+
+double[] numbers = {3.22, 4.2, 1.15, 77.15, 65.2};
+
+double max = numbers[0];
+double min = numbers[0];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    else if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+
+Console.WriteLine($"Минимальное число: {min}");
+Console.WriteLine($"Минимальное число: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+
+
